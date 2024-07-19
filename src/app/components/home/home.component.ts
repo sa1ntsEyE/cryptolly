e84456a9dc5abfc28d6087550bb7134f07d06372
Alexandr <stolbnyak2003@gmail.com>
2024-07-19 18:19:32 +0300

    the end page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  isOpen: boolean[] = [false, false, false, false, false, false, false, false];
+  questions = [
+    { title: 'What is the difference between Defi and Metaverse?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' },
+    { title: 'What should I do if Google verification fails to bind?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' },
+    { title: 'How to complete your KYC Verification ?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' },
+    { title: 'What should I do if I enter the Google verification code incorrectly?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' },
+    { title: 'Can We Enter in Website Without Login or Sign Up ?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' },
+    { title: 'What should I do if I lost the binded phone number ?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' },
+    { title: 'How to register an account on the web ?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' },
+    { title: 'How we Change mobile number ?', subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.' }
+  ];
+
+  handleOpen(index: number) {
+    this.isOpen[index] = !this.isOpen[index];
+  }
 }
